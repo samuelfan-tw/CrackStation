17fd3403c57c8cd9cdd42790377b4aa42391eaf0
TE-CHIA\samue <fante@oregonstate.edu>
2022-10-27 20:02:40 -0700

    add sha1 library in the CrackStation and check get correct password

--- Tests/CrackStationTests/CrackStationTests.swift
+++ Tests/CrackStationTests/CrackStationTests.swift
@@ -2,10 +2,15 @@ import XCTest
 @testable import CrackStation
 
 final class CrackStationTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(CrackStation().text, "Hello, World!")
+    func testCheckCorrectPassword() throws {
+        //Given
+        let input: String = "86f7e437faa5a7fce15d1ddcb9eaeaea377667b8" // 'a' sHA1
+        //When
+        let answer = CrackStation().CrackStation(password:input)
+        //Then
+        print(answer! as Any)
+        XCTAssertNotNil(answer)
+        XCTAssertEqual("a",answer)
     }
+
 }
